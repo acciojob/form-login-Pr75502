7e93327a62d0bbe89ea1bd1e606f031fb75da174
Pr75502 <pp1614582@gmail.com>
2025-03-09 21:29:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+	let first=document.querySelector("input[name='fname']")
+	let last=document.querySelector("input[name='lname']")
+	let btn=document.querySelector("#form1")
 
+	btn.addEventListener("submit",(e)=>{
+		e.preventDefault()
+			alert(`${first.value} ${last.value}`)
+	})
+	
 }
